e6ec21d65e7da517b58a36086fdfa6fedfe7671a
Joshua Estime <josh.n.estime@gmail.com>
2026-05-22 00:08:09 -0400

    created database, need to move main to database tho

--- app/main.py
+++ app/main.py
@@ -1,5 +1,20 @@
 from fastapi import FastAPI
 app=FastAPI()
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.models import Base
+DATABASE_URL="sqlite://./game.db" #the actual connection to the created game.db
+engine=create_engine(DATABASE_URL,connect_args={"check_same_thread:FALSE"})  #connection to database, connect_args=seperate threads for tings
+SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)#sesion maker, session=transactions/read and writes, autoocomit=false=dont auto comit,
+#autoflush=false stops from pushign qued objects to db b4 new query, bind=bind to engine/db created form b4
+Base.metadata.create_all(bind=engine)
+
+def get_db():#inject into route used later to create a new database session instance
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def root():
